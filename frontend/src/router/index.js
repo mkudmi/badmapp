96c2e3f76a5bce89cc9ae1e568b63a6d752968a1
Maksim <mku.dmi@gmail.com>
2024-05-19 14:08:40 +0200

    useless code has been removed

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/HomePage/Home.js'
-import About from '../pages/About.js'
 import Games from '../pages/Games/Games.js'
-import PlayerList from "../pages/Players.js"
-import PlayerCreate from "../pages/PlayerCreate.js"
-import PlayerEdit from "../pages/PlayerEdit.js"
 import OwnerLogin from "../pages/OwnerLogin/OwnerLogin.js"
 import GamesReg from "../pages/GamesReg/GamesReg.js"
 import AdminPanel from "../pages/AdminPanel/AdminPanel.js";
@@ -16,16 +12,14 @@ function MyRouter() {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/about-us' element={<About />} />
+
+            <Route path='/games/reg' element={<GamesReg />} />
+            <Route path='/owner/login' element={<OwnerLogin />} />
+
+            <Route path='/admin' element={<AdminPanel />} />
+
             <Route path='/games/info' element={<Games />} />
             <Route path='/games/create' element={<GameCreate />} />
-            <Route path='/players' element={<PlayerList />} />
-            <Route path='/players/create' element={<PlayerCreate />} />
-            <Route path='/player/:id/edit' element={<PlayerEdit />} />
-
-            <Route path='/owner/login' element={<OwnerLogin />} />
-            <Route path='/admin' element={<AdminPanel />} />
-            <Route path='/games/reg' element={<GamesReg />} />
         </Routes>
     )
 }
