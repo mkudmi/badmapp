4c003a921b5a8a8599b7c0d1c8adb7e2706ad55d
Maksim <mku.dmi@gmail.com>
2024-05-20 19:13:26 +0200

    added db scripts

--- database/data.sql
+++ database/data.sql
@@ -9,5 +9,27 @@ CREATE TABLE users(
     PRIMARY KEY (id)
 );
 
-INSERT INTO users (name, email, status)
-VALUES("Hans", "hans@mail.com", "active");
+CREATE TABLE owners(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    login VARCHAR(255) NOT NULL,
+    pass VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE games(
+    id INT NOT NULL AUTO_INCREMENT,
+    date timestamp NOT NULL,
+    owner VARCHAR(255) NOT NULL,
+    fields INT NOT NULL,
+    places BIGINT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE users_games(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    date timestamp NOT NULL,
+    PRIMARY KEY (id)
+);
