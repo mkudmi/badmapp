d57d00e68d42a54d8113ed0037abf03699079d11
Maksim <mku.dmi@gmail.com>
2024-05-20 19:24:17 +0200

    unreg button added

--- frontend/src/pages/HomePage/Home.js
+++ frontend/src/pages/HomePage/Home.js
@@ -9,6 +9,7 @@ function Home() {
             <h1>Badminton</h1>
             <div className="button-container">
                 <Link to="/games/reg" className="registration-button">Регистрация на игру</Link>
+                <Link to="/games/unreg" className="unreg-button">Сняться с регистрации</Link>
                 <Link to="/owner/login" className="login-button">Вход для владельцев</Link>
             </div>
         </div>
